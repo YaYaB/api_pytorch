c5bc6c0b9d0738d932212297f11fbef73cf7050f
YaYaB <bezzayassine@gmail.com>
2019-11-12 00:50:00 +0100

    Add examples to illustrate

---
+++ examples/example_2.sh
@@ -0,0 +1,6 @@
+# Create service
+curl -X PUT "localhost:8000/create" -d '{"model_name": "resnet18", "service_name": "s1"}'
+
+# Make prediction
+curl -X POST "localhost:8000/predict" -d '{"service_name": "s1", "urls_image":["trolpicture","https://images.daznservices.com/di/library/GOAL/99/e4/cristiano-ronaldo-juventus-2019-20_16jgivl5d7ean1cw4aymindbiy.jpg", "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg"]}'
+
---
+++ examples/example_3.sh
@@ -0,0 +1,12 @@
+# Create service 1
+curl -X PUT "localhost:8000/create" -d '{"model_name": "resnet18", "service_name": "s1"}'
+
+# Create service 2
+curl -X PUT "localhost:8000/create" -d '{"model_name": "wide_resnet101_2", "service_name": "s2"}'
+
+# Make prediction using model 1
+curl -X POST "localhost:8000/predict" -d '{"service_name": "s1", "urls_image":["https://images.daznservices.com/di/library/GOAL/99/e4/cristiano-ronaldo-juventus-2019-20_16jgivl5d7ean1cw4aymindbiy.jpg", "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg"]}'
+
+# Make prediction model 2
+curl -X POST "localhost:8000/predict" -d '{"service_name": "s2", "urls_image":["https://images.daznservices.com/di/library/GOAL/99/e4/cristiano-ronaldo-juventus-2019-20_16jgivl5d7ean1cw4aymindbiy.jpg", "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg"]}'
+

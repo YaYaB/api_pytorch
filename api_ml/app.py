954ca3de33c33040c6db015c5c6bf7bdea2dbae8
YaYaB <bezzayassine@gmail.com>
2019-11-12 22:14:13 +0100

    Route unknown routes to 404

--- api_ml/app.py
+++ api_ml/app.py
@@ -2,6 +2,7 @@ import falcon
 
 from api_ml import models_utils
 from .services import Services
+from api_ml.utils import DefaultRoute
 
 
 def create_app():
@@ -12,6 +13,9 @@ def create_app():
     api.add_route("/predict", models_utils.Predict(services))
     api.add_route("/available", models_utils.ListAvailableModels(services))
     api.add_route("/online", models_utils.ListOnlineModels(services))
+
+    default_route = DefaultRoute(api)
+    api.add_sink(default_route.on_get, '')
 
     return api
 
--- api_ml/utils.py
+++ api_ml/utils.py
@@ -1,49 +1,23 @@
-#import api_ml.models as models
-import torchvision.models as vision_models
-import torch
-from torchvision import transforms
+import falcon
 
 
-def load_model(name_model, weights=None):
-    if name_model == "resnet18":
-        model = vision_models.resnet18(pretrained=True)
-        model.eval()
+def list_routes(roots, parent=''):
+    elems = []
+    for root in roots:
+        elem = []
+        if root.method_map:
+            elem = [parent + '/' + root.raw_segment]
 
-        model = {
-            "model": model,
-            "preprocessing": transforms.Compose([
-                transforms.Resize(256),
-                transforms.CenterCrop(224),
-                transforms.ToTensor(),
-                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-            ]),
-            "postprocessing": lambda x: torch.nn.functional.softmax(x, dim=0),
-            "mapping": None,
-            "input_type": "image"
-        }
-
-        return model
-
-    elif name_model == "wide_resnet101_2":
-        model = vision_models.wide_resnet50_2(pretrained=True)
-        model.eval()
-
-        model = {
-            "model": model,
-            "preprocessing": transforms.Compose([
-                transforms.Resize(256),
-                transforms.CenterCrop(224),
-                transforms.ToTensor(),
-                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-            ]),
-            "postprocessing": lambda x: torch.nn.functional.softmax(x, dim=0),
-            "mapping": None,
-            "input_type": "image"
-        }
-        return model
+        if root.children:
+            return elem + list_routes(root.children, parent + '/' + root.raw_segment, verbose)
+        else:
+            elems += elem
+    return elems
 
 
-def batchify(iterable, batch_size=1):
-    length = len(iterable)
-    for ndx in range(0, length, batch_size):
-        yield iterable[ndx:min(ndx + batch_size, length)]
+class DefaultRoute(object):
+    def __init__(self, api):
+        self.list_routes = list_routes(api._router._roots)
+
+    def on_get(self, req, resp):
+        raise falcon.HTTPNotFound(description="Route not found. Possible routes are {}".format(self.list_routes))
